6e9da2cf4df2d9fc9c3ceff8783104d7475d238d
Wenish <Wenish@users.noreply.github.com>
2021-03-19 06:44:25 +0100

    adding stream icons

--- src/components/StreamLinks.vue
+++ src/components/StreamLinks.vue
@@ -1,42 +1,56 @@
 <template>
   <div class="container">
       <div class="item" v-for="item in items" :key="item.text">
+          <div v-is="item.icon" size="34"></div>
           <a :href="item.link" target="_blank">{{item.text}}</a>
       </div>
   </div>
 </template>
 
 <script>
+import IconApple from '@/components/IconApple'
+import IconInstagram from '@/components/IconInstagram'
+import IconSoundcloud from '@/components/IconSoundcloud'
+import IconSpotify from '@/components/IconSpotify'
+import IconYoutube from '@/components/IconYoutube'
+
 export default {
     name: 'StreamLinks',
+    components: {
+      IconApple,
+      IconInstagram,
+      IconSoundcloud,
+      IconSpotify,
+      IconYoutube
+    },
     data: () => ({
         items: [
           {
-            icon: 'mdi-instagram',
+            icon: 'IconInstagram',
             color: 'white darken-2',
             text: 'Instagram',
             link: 'https://www.instagram.com/johnvolo/'
           },
           {
-            icon: 'mdi-youtube-play',
+            icon: 'IconYoutube',
             color: 'red darken-2',
             text: 'Youtube',
             link: 'https://www.youtube.com/clangsoul'
           },
           {
-            icon: 'mdi-spotify',
+            icon: 'IconSpotify',
             color: 'teal darken-2',
             text: 'Spotify',
             link: 'https://open.spotify.com/artist/2UMPXGNnurwP45ae7lPdAG'
           },
           {
-            icon: 'mdi-apple',
+            icon: 'IconApple',
             color: 'grey darken-1',
             text: 'Apple Music',
             link: 'https://music.apple.com/us/artist/clangsoul/1488679288'
           },
           {
-            icon: 'mdi-soundcloud',
+            icon: 'IconSoundcloud',
             color: 'orange darken-2',
             text: 'Soundcloud',
             link: 'https://soundcloud.com/clangsoul'
@@ -62,5 +76,8 @@ export default {
 }
 .item {
   padding 10px 5px
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
